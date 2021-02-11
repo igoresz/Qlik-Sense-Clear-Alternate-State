95bcc4c56168268984a238589d0970d07dad7f6d
unknown <igoresz@mail.ru>
2021-02-11 11:21:44 +0300

    1

--- 1.bat
+++
@@ -1,6 +0,0 @@
-git init
-git add README.md
-git commit -m "v.1.01"
-git branch -M main
-git remote add origin https://github.com/igoresz/Qlik-Sense-Clear-Alternate-State.git
-git push -u origin main
